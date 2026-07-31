b229b149370bc47103dc96a4cae2b7ef25e8c616
VicenteDeAlmeida <vicentealmeida.devjava@gmail.com>
2026-07-31 14:08:29 -0300

    fix: Adiciona o @Column na minha classe e também adiciona o repository na classe Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
@@ -17,11 +17,21 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
     @Column(unique = true)
     private String email;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private Integer idade;
+    
     //@ManyToOne um ninja tem uma única missão
     @ManyToOne
     @JoinColumn(name = "missoes_id")
